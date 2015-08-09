781556d321df1b2538652eb17e9a2805a3b6f16e
johnprieto <jprieto001@regis.edu>
2015-08-09 14:25:30 -0700

    Update CascioPrieto-Assn4-common.h

--- CascioPrieto-Assn4-common.h
+++ CascioPrieto-Assn4-common.h
@@ -1,1 +1,19 @@
-#pragma once
+
+#ifndef PrietoCascio_Assn4_Common_h
+#define PrietoCascio_Assn4_Common_h
+#include<iostream>
+#include <string>
+using namespace std;
+
+//Define Constants
+const int NUM_MIN = 1;  //lower limit of random numbers
+const int NUM_MAX = 30000;  // upper limit of random numbers
+const int ARRAY_SIZE = 10000;   //max array size
+
+bool search(int tempNum, const int randArray[], int size);
+static int tempList[ARRAY_SIZE];
+void genRandomArray(int randArray[]);
+int getRndNum();
+void userMenu(char &choice1, char &choice2);
+
+#endif
