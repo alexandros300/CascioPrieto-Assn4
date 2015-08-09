afa45f9532f96e2cbfeeef2c26ca5fd96443ae11
alexandros300 <alexander.cascio@gmail.com>
2015-08-08 18:26:48 -0600

    Base commit

---
+++ CascioPrieto-Assn4-main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"CascioPrieto-Assn4-common.h"
+#include"Prieto-Assn4.h"
+#include"Cascio-Assn4.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int ret_code = 0;	//return/exit code for main function
+
+	return ret_code;
+}
