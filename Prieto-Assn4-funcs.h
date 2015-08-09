c13aaff8b9465ed818231fc55dfaa21c2397d17a
johnprieto <jprieto001@regis.edu>
2015-08-09 14:31:38 -0700

    Update and rename Prieto-Assn4.h to Prieto-Assn4-funcs.h

---
+++ Prieto-Assn4-funcs.h
@@ -0,0 +1,8 @@
+#ifndef Prieto_Assn4_funcs_h
+#define Prieto_Assn4_funcs_h
+
+int partition(int *sortArray, int p, int q);
+void mergesort(int *sortArray, int *tempList, int low, int high);
+void insertionSort(int *sortArray, int size);
+
+#endif
